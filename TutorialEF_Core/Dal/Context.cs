ca561f672c155d9a246ae597220f8530aa0ddd63
Diego Valerio <67572551+DiegoJVD@users.noreply.github.com>
2021-06-07 23:29:59 -0400

    Add project files.

---
+++ TutorialEF_Core/Dal/Context.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TutorialEF_Core.Models;
+
+namespace TutorialEF_Core.Dal
+{
+    public class SchoolContext : DbContext
+    {
+        public DbSet<Students> Student { get; set; }
+        public DbSet<Courses> Course { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=localhost;Database=SchoolDB;Trusted_Connection=True;");
+        }
+    }
+}
---
+++ TutorialEF_Core/Models/Courses.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TutorialEF_Core.Models
+{
+    public class Courses
+    {
+        [Key]
+        public int CourseId { get; set; }
+        public string CourseName { get; set; }
+    }
+}
